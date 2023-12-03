1f852bbf5b9ca3a227b2be1f04cee5f5ce084742
siam786e <shohrab.hossain36@gmail.com>
2023-12-03 14:53:59 +0600

    add fixed bug

--- database/migrations/2023_12_03_064704_add_quantity_to_products_table.php
+++ database/migrations/2023_12_03_064704_add_quantity_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->integer('quantity')->nullable()->after('price');
+            $table->integer('quantity')->nullable()->after('product_price');
         });
     }
 
